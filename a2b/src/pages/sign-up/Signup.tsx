0d5be49802a5e10017ab9faf573a82c3f04daed2
TF <tfalase24@gmail.com>
2023-08-23 16:24:44 +0100

    feat(...Signup.tsx): Created FC

---
+++ a2b/src/pages/sign-up/Signup.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Signup = () => {
+  return (
+    <>
+      <div>This is a sign up page for the applciation</div>
+      <div>
+        You enter your details and it will submit a form to our database
+      </div>
+      <div>
+        After a successful submission, it will take you to the Log in Page
+      </div>
+    </>
+  );
+};
+
+export default Signup;
