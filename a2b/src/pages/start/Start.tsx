08ea170ca46e3f5f20a82210b11912623d25008d
TF <tfalase24@gmail.com>
2023-08-23 17:15:52 +0100

    feat(...Start.tsx): Created a FC that will be the endpoint for the Start page

---
+++ a2b/src/pages/start/Start.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Start = () => {
+  return (
+    <>
+      <div>This is the start page for the application</div>
+      <div>This will hold the sign up and login page</div>
+    </>
+  );
+};
+
+export default Start;
