a9995f9b06d0b74af1da45be0c04e4ea626ca023
TF <tfalase24@gmail.com>
2023-08-23 15:54:18 +0100

    feat(.../Home.tsx): Created Home Component

---
+++ a2b/src/pages/home-page/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <div>This is the home-page for my website</div>
+      <div>So far, I want it to display the overview for the application</div>
+      <div>Also Quick access to saved routes and locations</div>
+      <div>
+        Most importantly the ability to capture a new geolocation snapshot
+      </div>
+      <div>Is this working</div>
+    </>
+  );
+};
+
+export default Home;
