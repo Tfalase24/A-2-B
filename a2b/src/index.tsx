4157de6f58b0b18a1d1d3ef8d1eb52beed3175d8
TF <tfalase24@gmail.com>
2023-08-23 15:52:03 +0100

    feat(a2b/src/index.tsx): Added Browser router for routing and suspense for lazy loading

--- a2b/src/index.tsx
+++ a2b/src/index.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const App = lazy(() => import('./App'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Suspense>
+        <App />
+      </Suspense>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
