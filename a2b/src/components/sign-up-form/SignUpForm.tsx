d026d763e2f569286addc0a7b43204bf96651bc0
TF <tfalase24@gmail.com>
2023-08-23 17:16:33 +0100

    feat(...SignUpForm.tsx): Created a FC that will be the Signup Form

---
+++ a2b/src/components/sign-up-form/SignUpForm.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import SubmitButton from './submit-button/SubmitButton';
+
+const SignUpForm = () => {
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log('You have clicked the submit button');
+  };
+  return (
+    <>
+      <form>
+        <div>
+          <input placeholder="Enter First Name..." />
+        </div>
+        <div>
+          <input placeholder="Enter Last Name..." />
+        </div>
+        <div>
+          <input placeholder="Enter Email Address..." />
+        </div>
+        <div>
+          <input placeholder="Choose Password..." type="password" />
+        </div>
+        <div>
+          <input placeholder="Verify Password..." type="password" />
+        </div>
+        <div>
+          <SubmitButton label="Submit" onClick={handleSubmit} />
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default SignUpForm;
