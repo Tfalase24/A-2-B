2fbe225ee5d0175fa962b17be7e1f503fc619d9d
TF <tfalase24@gmail.com>
2023-08-23 17:17:31 +0100

    feat(a2b/src/App.tsx): Created Routes for routing to endpoints

--- a2b/src/App.tsx
+++ a2b/src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { lazy } from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+const Start = lazy(() => import('./pages/start/Start'));
+const Home = lazy(() => import('./pages/home-page/Home'));
+const Signup = lazy(() => import('./pages/sign-up/Signup'));
+const Login = lazy(() => import('./pages/log-in/Login'));
+const SavedLocations = lazy(
+  () => import('./pages/saved-locations/Savedlocations')
+);
+const CreateARoute = lazy(() => import('./pages/create-a-route/CreateARoute'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Start />}></Route>
+      <Route path="signup" element={<Signup />}></Route>
+      <Route path="login" element={<Login />}></Route>
+      <Route path="/home" element={<Home />}></Route>
+      <Route path="saved-locations" element={<SavedLocations />}></Route>
+      <Route path="create-a-route" element={<CreateARoute />}></Route>
+    </Routes>
   );
 }
 
