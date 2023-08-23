46486da4e64929e0be3ec063aa778bd1e7c2b3f3
TF <tfalase24@gmail.com>
2023-08-23 16:24:03 +0100

    feat(...Login.tsx): Created FC

---
+++ a2b/src/pages/log-in/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <>
+      <div>This is a log in page that simply displays login input fields</div>
+      <div>If you login successfully it will direct you to the home page</div>
+    </>
+  );
+};
+
+export default Login;
