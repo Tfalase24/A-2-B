ae96ebd05b53cf54656e668210451ad5a1c06da3
TF <tfalase24@gmail.com>
2023-08-23 16:23:32 +0100

    feat(...CreateARoute.tsx): Created FC

---
+++ a2b/src/pages/create-a-route/CreateARoute.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const CreateARoute = () => {
+  return (
+    <>
+      <div>
+        This page will display all captured locations, and have a button to use
+        the current one, or search for one
+      </div>
+      <div>Then you can add your locations together to create a route</div>
+      <div>Also you will have the option to save your route if you want</div>
+      <div>And view saved routes by clicking a button on the side</div>
+    </>
+  );
+};
+
+export default CreateARoute;
