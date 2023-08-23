e84327fb97bbe747e3924fcdca8d4b509f9344fc
TF <tfalase24@gmail.com>
2023-08-23 16:24:24 +0100

    feat(...SavedLocations.tsx): Created FC

---
+++ a2b/src/pages/saved-locations/Savedlocations.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Savedlocations = () => {
+  return (
+    <>
+      <div>This page will display the saved locations</div>
+      <div>
+        This could also save specific routes once you click a separate tab
+      </div>
+    </>
+  );
+};
+
+export default Savedlocations;
