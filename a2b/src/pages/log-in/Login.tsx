00f0ab79665321ae72323170ab1268ead9d7b38a
TF <tfalase24@gmail.com>
2023-08-24 11:53:23 +0100

    ref(..log-in/Login.tsx): Added new Login Compontent FC and Submit button

--- a2b/src/pages/log-in/Login.tsx
+++ a2b/src/pages/log-in/Login.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import SubmitButton from '../../components/submit-button/SubmitButton';
+import LoginComponent from '../../components/login/Login';
 
 const Login = () => {
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log('The submit button has been clicked');
+  };
+
   return (
     <>
       <div>This is a log in page that simply displays login input fields</div>
       <div>If you login successfully it will direct you to the home page</div>
+      <LoginComponent />
+      <SubmitButton label="Submit" onClick={handleSubmit} />
     </>
   );
 };
