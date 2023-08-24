4d838190342febd4e70fbd7d97994e3d634480e2
TF <tfalase24@gmail.com>
2023-08-24 11:52:29 +0100

    feat(...Login.tsx): Created Login component

---
+++ a2b/src/components/login/Login.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LoginComponent = () => {
+  return (
+    <>
+      <div>
+        <input placeholder="Enter Email Address..." />
+      </div>
+      <div>
+        <input placeholder="Enter Password..." />
+      </div>
+    </>
+  );
+};
+
+export default LoginComponent;
